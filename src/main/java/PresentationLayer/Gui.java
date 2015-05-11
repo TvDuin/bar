968fa15892567257c16146cc6f0a3e0444f5d3c4
TvDuin <t.vanduin1@student.avans.nl>
2015-05-11 10:15:47 +0200

    Switched to intelliJ manual Jframe

---
+++ src/main/java/PresentationLayer/Gui.java
@@ -0,0 +1,15 @@
+package PresentationLayer;
+
+import javax.swing.*;
+
+/**
+ * Created by Thomas on 11-5-2015.
+ */
+public class Gui {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Hartige Hap - Bar");
+        frame.setSize(1280, 720);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
