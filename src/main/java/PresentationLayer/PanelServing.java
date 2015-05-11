4214c668aebb2a155b53707963f7da0fa32685f8
msfranke <msfranke@student.avans.nl>
2015-05-11 10:21:12 +0200

    10:21

---
+++ src/main/java/PresentationLayer/PanelServing.java
@@ -0,0 +1,17 @@
+package PresentationLayer;
+
+import javax.swing.*;
+
+/**
+ * Created by Maarten on 11-5-2015.
+ */
+public class PanelServing {
+    private JTable table1;
+    private JTable table2;
+    private JButton bestellingenButton;
+    private JButton tafelsButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/main/java/PresentationLayer/PanelTables.java
@@ -0,0 +1,12 @@
+package PresentationLayer;
+
+import javax.swing.*;
+
+/**
+ * Created by Maarten on 11-5-2015.
+ */
+public class PanelTables {
+    private JTable table1;
+    private JButton bestellingenButton;
+    private JButton tafelsButton;
+}
