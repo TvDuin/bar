e38af028c5a1550687d556e76417fc5615673ee2
TvDuin <t.vanduin1@student.avans.nl>
2015-06-12 08:25:07 +0200

    JUnit testclass added for ServingDAO -> used method = retrieveBeverages

---
+++ src/test/java/datastoragelayer/ServingDAOTest.java
@@ -0,0 +1,16 @@
+package datastoragelayer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Thomas on 12-6-2015.
+ */
+public class ServingDAOTest {
+
+    @Test
+    public void testRetrieveBeverages() throws Exception {
+
+    }
+}
